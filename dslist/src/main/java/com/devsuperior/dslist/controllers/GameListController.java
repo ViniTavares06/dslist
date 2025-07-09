cc9d0c5a5babc203eb2fc506ce3c35e96474fb74
ViniTavares06 <vinixz06@gmail.com>
2025-07-09 20:15:10 -0300

    feat: implementa controllers, services, repositories e DTOs para Game e GameList

---
+++ dslist/src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -0,0 +1,34 @@
+package com.devsuperior.dslist.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/lists")
+public class GameListController {
+
+    @Autowired
+    private GameListService gameListService;
+
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping
+    public List<GameListDTO> findAll() {
+        List<GameListDTO> result = gameListService.findAll();
+        return result;
+    }
+
+    @GetMapping(value = "/{listId}/games")
+    public List<GameMinDTO> findGames(@PathVariable Long listId) {
+        List<GameMinDTO> result = gameService.findByGameList(listId);
+        return result;
+    }
+
+    @PostMapping(value = "/{listId}/replacement")
+    public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body) {
+        gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
+    }
+}
